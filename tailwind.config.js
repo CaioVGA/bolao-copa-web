b139d9ce69b0a85235ba833fe3332d50b4737f26
CaioVGA <caio.v.araujo1997@hotmail.com>
2022-11-10 09:55:58 -0300

    new assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,23 +10,15 @@ module.exports = {
       },
 
       backgroundImage: {
-        app: 'url(/app-bg-2.png)',
+        app: 'url(/app-bg.png)',
       },
 
       colors:{
-        softgreen: {
-          500: '#129E57',
-        },
-
+        
         red: {
           300: '#B11544',
           600: '#8B1538',
           700: '#510D21',
-        },
-
-        yellow: {
-          500: '#F7DD43',
-          700: '#E5CD3D',
         },
 
         gray: {
